f365c2353689c8f0a6b0be9c6b262ef1ded7a557
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-12-27 20:04:44 +0000

    style: 💄 moves sizing styles to GalleryItem

--- lib/components/Galleries/GalleryItem/GalleryItem.tsx
+++ lib/components/Galleries/GalleryItem/GalleryItem.tsx
@@ -1,16 +1,21 @@
 import { ReactElement } from "react";
-import styles from './GalleryItem.module.css'
+import styles from './GalleryItem.module.css';
+import clsx from "clsx";
+
 
 export interface GalleryItemProps {
     imageUrl: string;
     altText: string;
     imageId: number;
+    size?: 'small' | 'medium' | 'large'
 }
 
-const GalleryItem = ({ imageUrl, altText, imageId }: GalleryItemProps): ReactElement => {
+const GalleryItem = ({ imageUrl, altText, imageId, size = 'medium'}: GalleryItemProps): ReactElement => {
+    const galleryStyles = clsx(styles.image, styles[size])
+
     return (
         <div id={`gallery-item-${imageId}`}>
-            <img className={styles.image} src={imageUrl} alt={altText} loading="lazy" />
+            <img className={galleryStyles} src={imageUrl} alt={altText} loading="lazy" />
         </div>
     );
 };
--- lib/components/Galleries/HorizontalGallery/HorizontalGallery.tsx
+++ lib/components/Galleries/HorizontalGallery/HorizontalGallery.tsx
@@ -1,20 +1,16 @@
 import { ReactElement } from "react";
 import GalleryItem, { GalleryItemProps } from "../GalleryItem/GalleryItem";
-import clsx from "clsx";
-import styles from "./HorizontalGallery.module.css"
+// import styles from "./HorizontalGallery.module.css"
 
 export interface HorizontalGalleryProps {
     images: GalleryItemProps[];
-    size?: 'small' | 'medium' | 'large'
 }
 
 const HorizontalGallery = ({
     images,
-    size = 'medium'
 }: HorizontalGalleryProps): ReactElement => {
-    const galleryStyles = clsx(styles.container, styles[size])
     return (
-            <div className={galleryStyles}>
+            <div >
                 {images &&
                     images.map((image) => (
                         <div key={image.imageId}>
