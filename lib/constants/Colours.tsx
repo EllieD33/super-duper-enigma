cd3429ec3aa779952507783985979160786cc47b
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2025-01-12 17:10:09 +0000

    style: 💄 adds colours enums

---
+++ lib/constants/Colours.tsx
@@ -0,0 +1,39 @@
+export enum Colours {
+    White = "#fffeff",
+    Black = "#000300",
+    Gray = "#2d3047",
+
+    Red = "#EF5350",
+    Blue = "#2C699A",
+    DarkBlue = "#073B4C",
+    Green = "#16DB93",
+    Yellow = "#EFEA5A",
+    Purple = "#54478C",
+    Orange = "#F29E4C",
+    Teal = "#0DB39E",
+    Pink = "#EF476F",
+}
+
+export enum ErrorColours {
+    Light = "#EF9A9A",
+    Main = "#F44336",
+    Dark = "#D32F2F",
+}
+
+export enum WarningColours {
+    Light = "#FFE082",
+    Main = "#FFC107",
+    Dark = "#FFA000",
+}
+
+export enum SuccessColours {
+    Light = "#A5D6A7",
+    Main = "#4CAF50",
+    Dark = "#388E3C",
+}
+
+export enum InfoColours {
+    Light = "#90CAF9",
+    Main = "#2196F3",
+    Dark = "#1565C0",
+}
